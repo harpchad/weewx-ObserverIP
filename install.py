329a378bb1a1e1fa786fe172098db3ae82dd27ba
Chad Harp <chad.harp@mac.com>
2022-05-03 12:34:47 -0500

    Add installer

---
+++ install.py
@@ -0,0 +1,22 @@
+from setup import ExtensionInstaller
+
+def loader():
+    return ObserverIPInstaller()
+
+class ObserverIPInstaller(ExtensionInstaller):
+    def __init__(self):
+        super(ObserverIPInstaller, self).__init__(
+                version="0.1",
+                name='observerip',
+                description='driver for Ambient ObserverIP',
+                author="Pat OBrien",
+                config={
+                    'ObserverIP': {
+                        'loop_interval': '15',
+                        'ip_address': '192.168.0.55',
+                        'hardware': 'Ambient Weather WS-1200-IP with ObserverIP',
+                        'driver': 'user.observerip',
+                    }
+                },
+                files=[('bin/user', ['bin/user/observerip.py'])]
+                )
